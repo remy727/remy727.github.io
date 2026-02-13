17a392e01a14a126964bfde93d840817967fc744
Remy Wang <remy.wang727@gmail.com>
2026-02-13 12:46:31 +0000

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://remy727.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
